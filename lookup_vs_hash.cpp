25a8d2d67fea1ea046d33cbc12beb501acef0940
Timir Karia <timir@capitalkpartners.com>
2013-05-27 12:53:42 +0000

    New test files for mmap file io and cout formatting

--- lookup_vs_hash.cpp
+++ lookup_vs_hash.cpp
@@ -20,10 +20,17 @@ void TestHash();
 void TestArray();
 void CreateKeys(char** ptr, int num_keys, int key_len);
 
+typedef char XXX[3+1];
+
 int 
 main(int argc, char** argv)
 {
+    srand(time(NULL));
     char* keys;  
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s %s %s\n", argv[0], "num keys", "key len");
+            return -1;
+        }
     CreateKeys(&keys, atoi(argv[1]), atoi(argv[2]));
 //  TestHash();
 //  TestArray();
@@ -41,22 +48,26 @@ struct eqstr
 char
 RandAsciiChar()
 {
-    int min = 'A';
-    int max = 'Z';
+    char min = 'A';
+    char max = 'Z';
     double x = rand()/static_cast<double>(RAND_MAX);
-    char ch = min + static_cast<char>(x*max - min);
+    char ch = min + static_cast<char>(x*(max - min));
     return ch;
 }
 
 void
-RandCharArray(char** ptr, int size)
+RandCharArray(char* ptr, int size)
 {
-    if (!ptr && *ptr) {
+    if (!ptr) {// && *ptr) {
         return;
     }
+    char ch;
     for(int i = 0; i< size; i++) {
-        *ptr[i] = RandAsciiChar();
-    }
+        ch = RandAsciiChar();
+        fprintf(stderr, "Char: %d \n", ch);
+        ptr[i] = ch;
+    } 
+    fprintf(stderr, "Generated: %s\n", ptr);
 }
 
 void
@@ -65,16 +76,23 @@ CreateKeys(char** ptr, int num_keys, int key_len)
     if (!ptr && *ptr) {
         return;
     }
-    *ptr = (char*)malloc(num_keys * sizeof(char[key_len + 1]));
+
+
+    int alloc_size = num_keys * (sizeof(char)*(key_len+1));
+    fprintf(stderr, "Key length: %d\n", key_len);
+    fprintf(stderr, "Num keys: %d\n", num_keys);
+    fprintf(stderr, "Allocating: %d bytes\n", alloc_size);
+    *ptr = (char*)malloc(alloc_size);
+
     for (int i = 0; i < num_keys; i++) {
-        RandCharArray(&(*ptr[i]), key_len);
-        &(ptr[i][key_len+1]) = NULL; // null terminate
-        fprintf(stderr, "%s\n", *ptr[i]);
+        RandCharArray((*ptr+(i*(key_len+1))), key_len);
+        (*ptr)[(i+1)*(key_len+1)-1] = '\0'; // null terminate
+        fprintf(stderr, "Key: %s\n", (*ptr)+(i*(key_len+1)));
     }
 }
 
 void
-TestHash();
+TestHash()
 {
   dense_hash_map<const char*, int, hash<const char*>, eqstr> months;
   
@@ -107,7 +125,7 @@ TestHash();
 
 
   // Set and map test
-   pair<int, int> p1(1, 1);
+  pair<int, int> p1(1, 1);
   pair<int, int> p2(1, 2);
   set< pair<int, int> > s;
   s.insert(p1);
